b195f6618580ab566bec44bec977dc8de366736d
Jason Huynh <htujason@gmail.com>
2023-02-20 20:04:18 -0800

    Finished Website

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function messaged() {
+            var name = document.getElementId('name');
+            var email = document.getElementId('email');
+            var email = document.getElementId('msg');
+            alert("Submitted. I'll get back to you when I can.");
+        }
